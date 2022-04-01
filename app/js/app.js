e2ba84d087512cfe05691c638672a6cc13e1caa3
deggion <deggiondeggion@gmail.com>
2022-04-02 01:30:54 +0300

    finished page cases

--- app/js/app.js
+++ app/js/app.js
@@ -81,6 +81,20 @@ document.addEventListener('DOMContentLoaded', () => {
 		$('html, body').removeClass('_over-hidden')
 	})
 
+	$('.case-template__nav li a').on('click', function() {
+
+		let href = $(this).attr('href');
+
+		$('html, body').animate({
+			scrollTop: $(href).offset().top
+		}, {
+			duration: 370,   // по умолчанию «400» 
+			easing: "linear" // по умолчанию «swing» 
+		});
+
+		return false;
+	});
+
 })
 
 
